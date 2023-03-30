334e66e0cbaaa3a2fd2e5f5b8724f551e31f2a26
manishaKhan1 <117927495+manishaKhan1@users.noreply.github.com>
2023-03-30 18:15:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,235 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// import './App.css';
+// import React, {useState, useEffect} from 'react'
+
+
+// const NEWS_YEARS_EVE= new Date(2023,3,30);
+
+
+// const getTimedelta=()=>{
+//   const now=new Date();
+//   console.log(now)
+//   const timedelta= NEWS_YEARS_EVE.getTime()-now.getTime();
+
+//   //diff in days
+//   const days=Math.floor(timedelta / (24 * 60 * 60 * 1000));
+
+//   //diff in hours
+//   const hours=Math.floor((timedelta % (24 * 60 * 60 * 1000))/(60*60*1000))
+
+//   //diff in minutes
+//   const minutes=Math.floor((timedelta % (60*60*1000)) /(60*1000))
+
+//   //diff in seconds
+//   const seconds=Math.floor((timedelta % (60 * 1000))/1000);
+
+  
+
+
+//   return{
+//     days,
+//     hours,
+//     minutes,
+//     seconds
+//   };
+// };
+
+
+
+
+
+
+// function App() {
+
+// const [counDown, setCounDown] = useState();
+// const[loading, setLoading]=useState(true);
+// const[isNewYear, setIsNewYear]=useState(false);
+
+// useEffect(()=>{
+//   const interval=setInterval(()=>{
+//     const timedelta=getTimedelta();
+
+//     setCounDown(timedelta);
+//     setLoading(false)
+
+//     if(timedelta.seconds<0){
+//       setIsNewYear(true)
+//     }
+//   }, 2000);
+
+//   return ()=> clearInterval(interval)
+// });
+
+// if(loading){
+//   return <h1>Loading...</h1>
+// }
+
+// if(isNewYear){
+//   return <h1>Happy New Year 2024</h1>
+// }
+
+//   return (
+//     <div className="container">
+
+//       <div className="flex">
+//         <span className="font-big">{counDown.days}</span>
+//         <span className="font-small">DAYS</span>
+//       </div>
+      
+//       <div className="flex">
+//         <span className="font-big">{counDown.hours}</span>
+//         <span className="font-small">HOURS</span>
+//       </div>
+
+//       <div className="flex">
+//         <span className="font-big">{counDown.minutes}</span>
+//         <span className="font-small">MINUTES</span>
+//       </div>
+
+//       <div className="flex">
+//         <span className="font-big">{counDown.seconds}</span>
+//         <span className="font-small">SECONDS</span>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // import './App.css';
+// // import React, {useState, useEffect} from 'react'
+
+
+// // const NEWS_YEARS_EVE= new Date(2024,11,31);
+
+
+// // const getTimedelta=()=>{
+// //   const now=new Date();
+// //   const timedelta= NEWS_YEARS_EVE.getTime()-now.getTime();
+
+// //   //diff in days
+// //   const days=Math.floor(timedelta / (24 * 60 * 60 * 1000));
+
+// //   //diff in hours
+// //   const hours=Math.floor((timedelta % (24 * 60 * 60 * 1000))/(60*60*1000))
+
+// //   //diff in minutes
+// //   const minutes=Math.floor((timedelta % (60*60*1000)) /(60*1000))
+
+// //   //diff in seconds
+// //   const seconds=Math.floor((timedelta % (60 * 1000))/1000);
+
+  
+
+
+// //   return{
+// //     days,
+// //     hours,
+// //     minutes,
+// //     seconds
+// //   };
+// // };
+
+
+
+
+
+
+// // function App() {
+
+// // const [counDown, setCounDown] = useState();
+// // const[loading, setLoading]=useState(true);
+// // const[isNewYear, setIsNewYear]=useState(false);
+
+// // useEffect(()=>{
+// //   const interval=setInterval(()=>{
+// //     const timedelta=getTimedelta();
+
+// //     setCounDown(timedelta);
+// //     setLoading(false)
+
+// //     if(timedelta.seconds<0){
+// //       setIsNewYear(true)
+// //     }
+// //   }, 2000);
+
+// //   return ()=> clearInterval(interval)
+// // });
+
+// // if(loading){
+// //   return <h1>Loading...</h1>
+// // }
+
+// // if(isNewYear){
+// //   return <h1>Happy New Year 2024</h1>
+// // }
+
+// //   return (
+// //     <div className="container">
+
+// //       <div className="flex">
+// //         <span className="font-big">{counDown.days}</span>
+// //         <span className="font-small">DAYS</span>
+// //       </div>
+      
+// //       <div className="flex">
+// //         <span className="font-big">{counDown.hours}</span>
+// //         <span className="font-small">HOURS</span>
+// //       </div>
+
+// //       <div className="flex">
+// //         <span className="font-big">{counDown.minutes}</span>
+// //         <span className="font-small">MINUTES</span>
+// //       </div>
+
+// //       <div className="flex">
+// //         <span className="font-big">{counDown.seconds}</span>
+// //         <span className="font-small">SECONDS</span>
+// //       </div>
+// //     </div>
+// //   );
+// // }
+
+// // export default App;
+
+
+
+
+
+
+import React from 'react'
+import Timer from './components/timer'
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
     </div>
-  );
+  )
 }
 
-export default App;
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,94 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Countdown from "react-countdown";
+import { useState, useEffect } from "react";
+
+// Random component
+const Completionist = () => <span>You are good to go.....</span>;
+
+// Renderer callback with condition
+const renderer = ({ hours, minutes, seconds, completed }) => {
+  if (completed) {
+    // Render a complete state
+    return <Completionist />;
+  } else {
+    // Render a countdown
+    return (
+      <span>
+        {hours}:{minutes}:{seconds}
+      </span>
+    );
+  }
+};
+
+const getLocalStorageValue = (s) => localStorage.getItem(s);
+
+const App = () => {
+  const [data, setData] = useState(
+    { date: Date.now(), delay: 60000 } //10 seconds
+  );
+  const wantedDelay = 60000; //10 ms
+
+  //[START] componentDidMount
+  //Code runs only one time after each reloading
+  useEffect(() => {
+    const savedDate = getLocalStorageValue("end_date");
+    if (savedDate != null && !isNaN(savedDate)) {
+      const currentTime = Date.now();
+      const delta = parseInt(savedDate, 10) - currentTime;
+
+      //Do you reach the end?
+      if (delta > wantedDelay) {
+        //Yes we clear uour saved end date
+        if (localStorage.getItem("end_date").length > 0)
+          localStorage.removeItem("end_date");
+      } else {
+        //No update the end date with the current date
+        setData({ date: currentTime, delay: delta });
+      }
+    }
+  }, []);
+  //[END] componentDidMount
+
+  return (
+    <div>
+      <Countdown
+        date={data.date + data.delay}
+        renderer={renderer}
+        onStart={(delta) => {
+          //Save the end date
+          if (localStorage.getItem("end_date") == null)
+            localStorage.setItem(
+              "end_date",
+              JSON.stringify(data.date + data.delay)
+            );
+        }}
+        onComplete={() => {
+          if (localStorage.getItem("end_date") != null)
+            localStorage.removeItem("end_date");
+        }}
+      />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
